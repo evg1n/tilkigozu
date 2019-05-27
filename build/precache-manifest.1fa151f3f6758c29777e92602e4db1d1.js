ec83ed88c35a9c6966ce74a100c0a15bb05036e3
evg1n <evginserbest@outlook.com>
2019-05-27 14:35:20 +0300

    build

---
+++ build/precache-manifest.1fa151f3f6758c29777e92602e4db1d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882a798b46dda62552a9232777d601b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91af6d1a1a70254a3d2b",
+    "url": "/static/css/main.0b9794e6.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "91af6d1a1a70254a3d2b",
+    "url": "/static/js/main.28647029.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
